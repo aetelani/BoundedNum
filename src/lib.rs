c7d8e29401bf29809984218da41d159af2cda8b0
Anssi Eteläniemi <anssi.etelaniemi@confidex.com>
2022-06-30 12:44:26 +0300

    Changed ..set_fn param to Self

--- src/lib.rs
+++ src/lib.rs
@@ -35,13 +35,11 @@ impl<const LOWER: Bound<&'static isize>, const UPPER: Bound<&'static isize>> Bou
         self.contains(&self.0)
     }
     fn get_bounds() -> Range<isize> {
-        let (start, end) =
-            if let (Included(start_ref), Excluded(end_ref)) = (LOWER, UPPER) {
-                (*start_ref, *end_ref)
-            } else {
-                todo!()
-            };
-        Range { start, end }
+        match (LOWER, UPPER) {
+            (Included(start_ref), Excluded(end_ref)) => Range{start: *start_ref, end: *end_ref },
+
+            // Pooping todos
+            (_,_) => { todo!() } }
     }
 
     fn try_get(&self) -> Result<isize, BoundedIntErr> {
@@ -60,17 +58,17 @@ impl<const LOWER: Bound<&'static isize>, const UPPER: Bound<&'static isize>> Bou
             Err(BoundError)
         }
     }
-    fn try_set_fn(&mut self, set_with_fn: &impl Fn(isize) -> isize) -> Result<isize, BoundedIntErr>
+    fn try_set_fn(&mut self, set_with_fn: &impl Fn(&mut Self)) -> Result<isize, BoundedIntErr>
     {
-        let new_value = set_with_fn(self.0);
-        if self.contains(&new_value) {
-            self.0 = new_value;
+        set_with_fn(self);
+        if self.is_valid() {
             Ok(self.0)
         } else {
             // Value not updated
             Err(BoundError)
         }
     }
+    // Not sure if once invalidated should be able to be validated, Reset to Default value?
     fn invalidate(&mut self) {
         // is_valid() is now false
         if let Excluded(v) = UPPER {
@@ -86,8 +84,8 @@ impl UncheckedIntTrait for BoundedInt {
         self.0 = new_value;
         self.0
     }
-    unsafe fn set_fn(&mut self, set_with_fn: &impl Fn(isize) -> isize) -> isize {
-        self.0 = set_with_fn(self.0);
+    unsafe fn set_fn(&mut self, set_with_fn: &impl Fn(&mut Self)) -> isize {
+        set_with_fn(self);
         self.0
     }
 
@@ -98,7 +96,7 @@ impl UncheckedIntTrait for BoundedInt {
 
 pub trait UncheckedIntTrait {
     unsafe fn set(&mut self, new_value: isize) -> isize;
-    unsafe fn set_fn(&mut self, set_with_fn: &impl Fn(isize) -> isize) -> isize;
+    unsafe fn set_fn(&mut self, set_with_fn: &impl Fn(&mut Self)) -> isize;
     unsafe fn get(&self) -> isize;
 }
 
@@ -107,7 +105,10 @@ pub trait BoundedIntTrait {
     fn get_bounds() -> Range<isize>;
     fn try_get(&self) -> Result<isize, BoundedIntErr>;
     fn try_set(&mut self, new_value: isize) -> Result<isize, BoundedIntErr>;
-    fn try_set_fn(&mut self, set_with_fn: &impl Fn(isize) -> isize) -> Result<isize, BoundedIntErr>;
+    fn try_set_fn(&mut self, set_with_fn: &impl Fn(&mut Self)) -> Result<isize, BoundedIntErr>;
+
+    // Can be implemented only if Upper bound is Exclusive
+    // Bug when using Bound<_>
     fn invalidate(&mut self);
 }
 
@@ -137,7 +138,7 @@ mod tests {
         b.iter(|| unsafe {
             for i in -2000_isize..2000 {
                 // Optimized out or 1.9 ns/iter with black box
-                let _ = t.set_fn(&|v| v + i);
+                let _ = t.set_fn(&|mut v| {v.set(i);});
             }
         });
     }
@@ -149,7 +150,8 @@ mod tests {
         b.iter(|| unsafe {
             for i in -2000..2000 {
                 // Optimized out or 1.9 ns/iter with black box
-                let _ = t.set_fn(&|v| black_box(v + i));
+                // Fn panics as goes under MIN bounds with due negative i
+                let _ = t.set_fn(&|v| black_box({v.set(i);}));
             }
         });
     }
@@ -173,7 +175,7 @@ mod tests {
         b.iter(|| {
             for i in -2000_isize..2000 {
                 // Optimized out or 1.9 ns/iter with black box
-                let _ = t.try_set_fn(&|v| v + i).ok();
+                let _ = t.try_set_fn(&|mut v| {let _ = v.try_set(i);});
             }
         });
         /*let mut t = BoundedInt::<{ isize::MIN }>(isize::MIN);
